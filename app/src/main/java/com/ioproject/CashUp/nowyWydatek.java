917d14ad04e804f993f3c8556620aee216c9b52c
maciejkutyla <maciejo117@gmail.com>
2019-12-15 19:48:05 +0100

    dochod & wydatek 2

---
+++ app/src/main/java/com/ioproject/CashUp/nowyWydatek.java
@@ -0,0 +1,16 @@
+package com.ioproject.CashUp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class nowyWydatek extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        getSupportActionBar().hide();
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nowy_wydatek);
+
+    }
+}
