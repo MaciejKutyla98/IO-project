5fa2ae8add4a28f21b460a60e6dee84a97c7c2f6
maciejkutyla <maciejo117@gmail.com>
2019-12-15 19:44:21 +0100

    dochod & wydatek

--- app/src/main/java/com/ioproject/CashUp/LoginFields.java
+++ app/src/main/java/com/ioproject/CashUp/LoginFields.java
@@ -61,8 +61,6 @@ public class LoginFields extends AppCompatActivity {
     }
 
     public void home(View view) {
-        //EditText inputLogin = (EditText) findViewById(R.id.login);
-        //String login = inputLogin.getText().toString();
         Intent intent_home= new Intent(this, Home.class);
         intent_home.putExtra("nazwaUzytkownika", this.login);
         startActivity(intent_home);
